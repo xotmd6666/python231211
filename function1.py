c1339c19b6c85e9ae6a2a5499fb87bbc0c2e7b78
Taeseung Kim <xotmd6666@naver.com>
2023-12-11 16:36:26 +0900

    함수를 약간 수정

--- function1.py
+++ function1.py
@@ -1,5 +1,18 @@
 # function1.py
-def times(a,b):
+#기본값을 주는 경우
+def times(a=10,b=20):
     return a*b
 
-print(times(3,4))
+#함수를 호출
+print(times())
+print(times(5))
+print(times(5,6))
+
+#키워드인자(매개변수명을 자세하게 기술)
+def connectURI(server, port):
+    strURL = "http://" + server + ":" + port
+    return strURL
+
+#호출
+print(connectURI("multi.com", "80"))
+print(connectURI(port="8080", server="multi.com"))
