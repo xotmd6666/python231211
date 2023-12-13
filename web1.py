465f01abeb7607dff964b11be91800dab1397c48
Taeseung Kim <xotmd6666@naver.com>
2023-12-13 16:49:35 +0900

    웹 크롤링 연습1

---
+++ web1.py
@@ -0,0 +1,20 @@
+# web1.py
+#웹크롤링을 위한 선언
+from bs4 import BeautifulSoup
+
+
+#웹페이지를 로딩(전체 문서를 하나의 문자열 변수로 담기)
+#메서드체인(함수나 메서드를 한번에 호출)
+page = open("test01.html", "rt", encoding="utf-8").read()
+
+#검색이 용이한 객체
+soup = BeautifulSoup(page, "html.parser")
+
+#전체 문서
+#print(soup.prettify())
+#<p>태그 전부 검색
+#print(soup.find_all("p"))
+#첫번째<p>만 검색
+# print(soup.find("p"))
+#필터링:<p class="outer-text">
+print(soup.find_all("p", class_="outer-text"))
